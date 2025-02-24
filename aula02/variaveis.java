0c7daef985af17b320528d2034b53f16c6ac2584
Gustavo Sobral <gustavosobral1711@gmail.com>
2025-02-24 09:30:09 -0300

    Aula sobre variaveis

---
+++ aula02/variaveis.java
@@ -0,0 +1,27 @@
+public class variaveis {
+    public static void main(String[] args) {
+        String Name = "Gustavo";//primeiro vem o tipo, dps o nome da variavel, igual, e o valor
+        float Quebrado = 15.2f;//Tipo NomeDaVariavel  = valor
+        int Normal = 15;
+        boolean EuGozei = true;
+        String MyName = " Sobral";
+        String NomeInt = (Name + MyName);
+        char l1 = 'G';
+        char l2 = 'O';
+        char l3 = 'Z';
+        char l4 = 'E';
+        char l5 = 'I';
+        float soma = (Normal + Quebrado);
+        System.out.println(Name);
+        System.out.println(Quebrado);
+        System.out.println(Normal);
+        System.out.println(EuGozei);
+        System.out.println(soma);
+        System.out.println(NomeInt);
+        System.out.println(l1);
+        System.out.println(l2);
+        System.out.println(l3);
+        System.out.println(l4);
+        System.out.println(l5);
+    }
+}
