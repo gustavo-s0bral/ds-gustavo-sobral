e3ed9a5ec0551ab30872988d7db72d0337061bb3
Gustavo Sobral <gustavosobral1711@gmail.com>
2025-03-27 16:01:14 -0300

    Adiciona opção de escolher a ordem dos numeros

--- exerciciosExtras/exercicio02.java
+++ exerciciosExtras/exercicio02.java
@@ -5,13 +5,43 @@ public class exercicio02 {
         Scanner scanner1 = new Scanner(System.in);
         System.out.println("Escreva um numero aleatório maior que 0: ");
         int n1 = scanner1.nextInt();
+        System.out.println("Escolha se você quer em ordem crescente, decrescente ou ambas: ");
+        String ordem = scanner1.next();
         scanner1.close();
 
         if (n1 > 0) {
-            while (n1 > 0) {
-                System.out.println(n1);
+            if (ordem.equalsIgnoreCase("decrescente")) {
+                System.out.println("\nOrdem decrescente: ");
 
-                n1 = n1 - 1;
+                while (n1 >= 0) {
+                    System.out.println(n1);
+    
+                    n1 = n1 - 1;
+                }
+            }
+            if (ordem.equalsIgnoreCase("crescente")) {
+                System.out.println("\nOrdem crescente");
+                
+                for (int i = 0; i < n1; i++) {
+                    System.out.println(i);
+                }
+            }
+            if (ordem.equalsIgnoreCase("ambas")) {
+                System.out.println("\nOrdem decrescente: ");
+
+                int temp = n1;
+                while (n1 >= 0) {
+                    System.out.println(n1);
+    
+                    n1 = n1 - 1;
+                }
+
+                n1 = temp;
+                System.out.println("\nOrdem crescente: ");
+                
+                for (int i = 0; i < n1; i++) {
+                    System.out.println(i);
+                }
             }
         }
         else{
